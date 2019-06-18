24189eb33459b4664f0bee95920b8909b896207d
hareesp <hareesp@eightfold.ai>
2019-06-18 16:28:50 -0700

    css for page navigation

--- src/components/presentationalComponents/PaginationComponent.js
+++ src/components/presentationalComponents/PaginationComponent.js
@@ -7,13 +7,13 @@ const PaginationComponent = (props) => {
     <div className="clearfix m-5">
       <nav aria-label="Page navigation">
         <ul className="pagination float-right">
-          <li className={isPrevDisabled ? 'page-item disabled': 'page-item'}>
+          <li className={isPrevDisabled ? 'page-item disabled': 'page-item text-primary'}>
             <a className="page-link"  onClick={prev} aria-label="Previous">
               <span aria-hidden="true" >&laquo;</span>
               <span className="sr-only" >Previous</span>
             </a>
           </li>
-          <li className={isNextDisabled ? 'page-item disabled': 'page-item'}>
+          <li className={isNextDisabled ? 'page-item disabled': 'page-item text-primary'}>
             <a className="page-link"  onClick={next} aria-label="Next">
               <span aria-hidden="true" >&raquo;</span>
               <span className="sr-only" >Next</span>

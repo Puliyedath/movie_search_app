bfac6b610092c27127b8740b34140b8116f723bd
hpuliyedath <hpuliyedath@vlocity.com>
2019-06-18 22:54:58 -0700

    fixing the launch script more details about the app in readme

--- lauch_movie_search_app.sh
+++ lauch_movie_search_app.sh
@@ -2,26 +2,26 @@
 
 echo "Launching Movie Search App........"
 echo "Installing dependencies..........."
-`npm i`
+npm i
 
 echo ' '
 echo "####################################"
 echo ' '
 
 echo "Creating dist resources in dev mode"
-`npm run build:dev`
+npm run build:dev
 
 echo ' '
 echo "####################################"
 echo ' '
 
 echo "Running integration test.......... "
-`npm run integration:test`
+npm run integration:test
 
 echo ' '
 echo "####################################"
 echo ' '
 
 echo "Starting web pack dev server on port 8080"
-`npm start:dev:server`
+npm run start:dev:server
 echo "Launching the website on localhost:8080"

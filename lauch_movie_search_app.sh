f74ab7d79d6d8d0721e3ef91da59d5da91419ebd
hpuliyedath <hpuliyedath@vlocity.com>
2019-06-18 22:43:46 -0700

    add test execution as well

--- lauch_movie_search_app.sh
+++ lauch_movie_search_app.sh
@@ -1,10 +1,26 @@
 #!/bin/sh
 
 echo "Launching Movie Search App........"
+echo "Installing dependencies..........."
+`npm i`
+
+echo ' '
+echo "####################################"
+echo ' '
+
 echo "Creating dist resources in dev mode"
 `npm run build:dev`
 
+echo ' '
 echo "####################################"
+echo ' '
+
+echo "Running integration test.......... "
+`npm run integration:test`
+
+echo ' '
+echo "####################################"
+echo ' '
 
 echo "Starting web pack dev server on port 8080"
 `npm start:dev:server`
